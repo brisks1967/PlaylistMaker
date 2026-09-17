7896dd4a3286ffa913f1fa763558d8b44e70f1c8
brisks1967 <brisks@mail.ru>
2026-09-17 16:32:15 +0300

    Спринт13 170926

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -6,5 +6,16 @@ data class Track(
     val trackTimeMillis: Long, // Продолжительность трека в милисекундах
     val artworkUrl100: String?, // Ссылка на изображение обложки
     val trackId: Long,          // ID трека
-    val mark: Int  = 0  // метка 1 - из истории, 0 - не из истории
+    val mark: Int  = 0,  // метка 1 - из истории, 0 - не из истории
+
+    val collectionName: String?, // Название Альбома
+    val country: String?, // Название Страны
+    val primaryGenreName: String?, // Название Жанра
+    val releaseDate: String? // Год релиза
 )
+{
+    // замена URL - картинки 100х100 на 512х512
+    fun getCoverArtwork(): String? {
+        return artworkUrl100?.replaceAfterLast('/', "512x512bb.jpg")
+    }
+}
